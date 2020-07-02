a7071b7431d9853638dc68d647a176e026cb60f8
slykost <konstantynov.va@gmail.com>
2020-07-02 05:39:41 +0300

    DZ Lesson_07

---
+++ DZ_16.py
@@ -0,0 +1,14 @@
+# ДЗ 16. Функцию is_year_leap
+
+# Написать функцию is_year_leap, принимающую 1 аргумент — год,
+# и возвращающую True, если год високосный, и False иначе.
+
+
+def is_year_leap(year):
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        return True
+    else:
+        return False
+
+
+print(is_year_leap(int(input('Введите год: '))))
